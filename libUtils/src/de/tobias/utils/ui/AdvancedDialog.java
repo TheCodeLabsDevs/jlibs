fc03817fb464579461b56090458014309f181aa1
tobias <tobias.ullerich@icloud.com>
2016-08-29 23:29:01 +0200

    Add simple Diaglo with checkbox

---
+++ libUtils/src/de/tobias/utils/ui/AdvancedDialog.java
@@ -0,0 +1,52 @@
+package de.tobias.utils.ui;
+
+import java.util.Optional;
+
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.Dialog;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.layout.VBox;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+import javafx.stage.Window;
+
+public class AdvancedDialog extends Dialog<ButtonType> {
+
+	private CheckBox checkBox;
+	private Label contentLabel;
+
+	public AdvancedDialog(Window owner) {
+		contentLabel = new Label();
+		checkBox = new CheckBox();
+		VBox vBox = new VBox(14, contentLabel, checkBox);
+
+		getDialogPane().setContent(vBox);
+
+		initOwner(owner);
+		initModality(Modality.WINDOW_MODAL);
+	}
+
+	public void setContent(String text) {
+		contentLabel.setText(text);
+	}
+	
+	public void addButtonType(ButtonType button) {
+		getDialogPane().getButtonTypes().add(button);
+	}
+
+	public void setIcon(Image image) {
+		Stage dialogStage = (Stage) getDialogPane().getScene().getWindow();
+		dialogStage.getIcons().add(image);
+	}
+
+	public void setIcon(Optional<Image> image) {
+		Stage dialogStage = (Stage) getDialogPane().getScene().getWindow();
+		image.ifPresent(dialogStage.getIcons()::add);
+	}
+
+	public boolean isIgnoreUpdate() {
+		return checkBox.isSelected();
+	}
+}
