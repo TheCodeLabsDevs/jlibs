0b84ebc912409ce1762c40cac7463fd268dbe7d8
tobias <thinkdifferent055@gmail.com>
2018-05-28 19:33:11 +0200

    Add FadeAnimator (Close #28)

---
+++ libUtils/src/de/tobias/utils/nui/animation/FadeAnimator.java
@@ -0,0 +1,14 @@
+package de.tobias.utils.nui.animation;
+
+import javafx.animation.FadeTransition;
+import javafx.scene.Node;
+import javafx.util.Duration;
+
+public class FadeAnimator {
+	public static void fade(Node node, double from, double to, Duration duration) {
+		FadeTransition transition = new FadeTransition(duration, node);
+		transition.setFromValue(from);
+		transition.setToValue(to);
+		transition.playFromStart();
+	}
+}
