e9afb1525d7f08e547ad061df7a6bfd44c37b1e4
tobias <thinkdifferent055@gmail.com>
2018-07-19 22:05:46 +0200

    Add util classes to update

---
+++ Updater/src/main/java/de/tobias/updater/client/UpdateItem.java
@@ -0,0 +1,58 @@
+package de.tobias.updater.client;
+
+import de.tobias.utils.settings.Key;
+import de.tobias.utils.settings.SettingsSerializable;
+
+public class UpdateItem implements SettingsSerializable {
+
+	@Key
+	private int build;
+	@Key
+	private String version;
+	@Key
+	private String pathJar;
+	@Key
+	private String pathExe;
+	@Key
+	private String pathApp;
+
+	public int getBuild() {
+		return build;
+	}
+
+	public void setBuild(int build) {
+		this.build = build;
+	}
+
+	public String getVersion() {
+		return version;
+	}
+
+	public void setVersion(String version) {
+		this.version = version;
+	}
+
+	public String getPathJar() {
+		return pathJar;
+	}
+
+	public void setPathJar(String pathJar) {
+		this.pathJar = pathJar;
+	}
+
+	public String getPathExe() {
+		return pathExe;
+	}
+
+	public void setPathExe(String pathExe) {
+		this.pathExe = pathExe;
+	}
+
+	public String getPathApp() {
+		return pathApp;
+	}
+
+	public void setPathApp(String pathApp) {
+		this.pathApp = pathApp;
+	}
+}
