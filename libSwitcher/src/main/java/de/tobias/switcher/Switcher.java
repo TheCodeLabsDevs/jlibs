cd478748248c83cece0e5c64f68f6fe3a984233f
tobias <thinkdifferent055@gmail.com>
2018-09-25 12:59:33 +0200

    Handle init, deinit, connect for native

---
+++ libSwitcher/src/main/java/de/tobias/switcher/Switcher.java
@@ -0,0 +1,6 @@
+package de.tobias.switcher;
+
+public interface Switcher
+{
+	void connect();
+}
---
+++ libSwitcher/src/main/java/de/tobias/switcher/impl/native_mac/NativeMacSwitcher.java
@@ -0,0 +1,50 @@
+package de.tobias.switcher.impl.native_mac;
+
+import de.tobias.switcher.Switcher;
+
+public class NativeMacSwitcher implements Switcher
+{
+	private long nativePointer;
+
+	private long getNativePointer()
+	{
+		return nativePointer;
+	}
+
+	private void setNativePointer(long nativePointer)
+	{
+		this.nativePointer = nativePointer;
+	}
+
+	public NativeMacSwitcher()
+	{
+		init();
+		System.out.println(nativePointer);
+	}
+
+	private native void init();
+
+	private native void deinit();
+
+	@Override
+	protected void finalize() throws Throwable
+	{
+		System.out.println("finalize");
+		deinit();
+		super.finalize();
+	}
+
+	public void dispose() {
+		deinit();
+	}
+
+	public native void connect();
+
+	@Override
+	public String toString()
+	{
+		return "NativeMacSwitcher{" +
+				"nativePointer=" + nativePointer +
+				'}';
+	}
+}
