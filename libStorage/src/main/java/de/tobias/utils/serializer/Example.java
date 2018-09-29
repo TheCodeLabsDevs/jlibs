faffdabbb03c42ece6900c416b5a8e1203152bfb
Robert Goldmann <deadlocker@gmx.de>
2018-09-29 18:22:27 +0200

    added Serializer from deadlocker8 to libStorage

---
+++ libStorage/src/main/java/de/tobias/utils/serializer/Example.java
@@ -0,0 +1,58 @@
+package de.tobias.utils.serializer;
+
+import java.io.File;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Example
+{
+	private static final File SAVE_PATH = new File("data.dat");
+
+	public static void main(String[] args)
+	{
+		String text = "Lorem Ipsum";
+		String number = "5";
+		String text2 = "AABBCCDDEE";
+
+		Header header = new Header(1);
+		header.addElement(text);
+		header.addElement(number);
+		header.addElement(text2);
+
+		System.out.println("Header: " + header.createHeader());
+
+		String completeText = text + number + text2;
+		completeText = header.createHeader() + completeText;
+
+		System.out.println("Complete Text: " + completeText);
+
+		try
+		{
+			byte[] b = completeText.getBytes(StandardCharsets.UTF_8);
+			System.out.println("bytes: " + Arrays.toString(b));
+
+			Serializer.serializeToFile(SAVE_PATH.getAbsolutePath(), b);
+
+			//Deserialize
+			String data = new String(Serializer.deserializeFromFile(SAVE_PATH.getAbsolutePath()));
+			header = new Header(data);
+
+			ArrayList<String> parts = new ArrayList<>();
+			int position = header.getHeaderSize();
+
+			for(int i = 0; i < header.getNumberOfElements(); i++)
+			{
+				int currentSize = header.getHeaderPart(i);
+				parts.add(data.substring(position, position + currentSize));
+				position =  position + currentSize;
+			}
+
+			System.out.println(parts);
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+	}
+}
