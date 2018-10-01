4e375112fb682af1ec752974478f3f8d61315433
tobias <thinkdifferent055@gmail.com>
2018-10-01 16:03:44 +0200

    Improve codesign script

--- libMidi/scripts/pre-deploy.sh
+++ libMidi/scripts/pre-deploy.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
-codesign --remove-signature src/main/resources/libraries/liblibMidi.dylib
-
-echo BLA
+if hash codesign 2>/dev/null; then
+    codesign --remove-signature src/main/resources/libraries/liblibMidi.dylib
+    echo Remove code siging
+else
+    (>&2 echo "codesign image not found")
+fi
