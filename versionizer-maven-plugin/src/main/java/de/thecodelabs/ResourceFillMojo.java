868f3f6663ef85d7fbbbd5258ba6153962f8a883
tobias <thinkdifferent055@gmail.com>
2018-11-06 08:13:35 +0100

    Change distribution repository

--- versionizer-maven-plugin/src/main/java/de/thecodelabs/ResourceFillMojo.java
+++ versionizer-maven-plugin/src/main/java/de/thecodelabs/ResourceFillMojo.java
@@ -8,24 +8,13 @@ import org.apache.maven.project.MavenProject;
 
 import java.io.File;
 
-/**
- * Goal which touches a timestamp file.
- *
- * @goal touch
- * @phase process-sources
- */
 @Mojo(name = "resource-fill")
 public class ResourceFillMojo extends AbstractMojo
 {
-	/**
-	 * Location of the file.
-	 *
-	 * @required
-	 */
 	@Parameter(property = "resourceFile")
 	private File resourceFile;
 
-	@Parameter(defaultValue="${project}", readonly=true, required=true)
+	@Parameter(defaultValue = "${project}", readonly = true, required = true)
 	private MavenProject mavenProject;
 
 	public void execute() throws MojoExecutionException
