778c4cdb97c7aba0965da1320c12e909781adfca
Robert Goldmann <deadlocker@gmx.de>
2018-11-06 19:54:12 +0100

    improved unit tests

--- Versionizer/api/src/test/java/de/thecodelabs/versionizer/VersionTokenizerTest.java
+++ Versionizer/api/src/test/java/de/thecodelabs/versionizer/VersionTokenizerTest.java
@@ -1,12 +1,43 @@
 package de.thecodelabs.versionizer;
 
+import de.thecodelabs.versionizer.model.Version;
 import de.thecodelabs.versionizer.service.VersionTokenizer;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
 
 public class VersionTokenizerTest
 {
-	public static void main(String[] args)
+	@Test
+	public void testGetVersion()
 	{
-		System.out.println(VersionTokenizer.getVersion(null,"1.1.0-SNAPSHOT"));
-		System.out.println(VersionTokenizer.getVersion(null, "1.1.2"));
+		Version expected = new Version(null, 1, 1, 2, false);
+		assertEquals(expected, VersionTokenizer.getVersion(null, "1.1.2"));
+	}
+	@Test
+	public void testGetVersionMultipleDigits()
+	{
+		Version expected = new Version(null, 1, 100, 2, false);
+		assertEquals(expected, VersionTokenizer.getVersion(null, "1.100.2"));
+	}
+
+	@Test
+	public void testGetVersionWithSnapshot()
+	{
+		Version expected = new Version(null, 1, 1, 0, true);
+		assertEquals(expected, VersionTokenizer.getVersion(null, "1.1.0-SNAPSHOT"));
+	}
+
+	@Test
+	public void testGetVersionInvalid()
+	{
+		assertNull(VersionTokenizer.getVersion(null, "1.0"));
+	}
+
+	@Test
+	public void testGetVersionInvalid2()
+	{
+		assertNull(VersionTokenizer.getVersion(null, "xyz"));
 	}
 }
