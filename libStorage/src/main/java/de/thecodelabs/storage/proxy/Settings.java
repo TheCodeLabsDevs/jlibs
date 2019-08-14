cf935d547038b92906fe0ee96285f6474f25148a
tobias <thinkdifferent055@gmail.com>
2019-08-14 10:17:33 +0200

    Add custom path handling to settings proxy

--- libStorage/src/main/java/de/thecodelabs/storage/proxy/Settings.java
+++ libStorage/src/main/java/de/thecodelabs/storage/proxy/Settings.java
@@ -1,8 +1,12 @@
 package de.thecodelabs.storage.proxy;
 
+import java.nio.file.Path;
+
 public interface Settings
 {
 	void load();
 
 	void save();
+
+	Path getPath();
 }
--- libStorage/src/main/java/de/thecodelabs/storage/proxy/SettingsProxy.java
+++ libStorage/src/main/java/de/thecodelabs/storage/proxy/SettingsProxy.java
@@ -4,10 +4,8 @@ import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 import de.thecodelabs.storage.settings.Storage;
 
-import java.lang.reflect.InvocationHandler;
-import java.lang.reflect.Method;
-import java.lang.reflect.Proxy;
-import java.lang.reflect.Type;
+import java.lang.invoke.MethodHandles;
+import java.lang.reflect.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.HashMap;
@@ -45,16 +43,43 @@ public class SettingsProxy
 					@Override
 					public Object invoke(Object proxy, Method method, Object[] args) throws Throwable
 					{
+						if(method.getName().equals("getPath"))
+						{
+							Constructor<MethodHandles.Lookup> constructor;
+							Class<?> declaringClass;
+							Object result;
+
+							if(method.isDefault())
+							{
+								declaringClass = method.getDeclaringClass();
+								constructor = MethodHandles.Lookup.class.getDeclaredConstructor(Class.class, int.class);
+
+								constructor.setAccessible(true);
+
+								result = constructor.
+										newInstance(declaringClass, MethodHandles.Lookup.PRIVATE).
+										unreflectSpecial(method, declaringClass).
+										bindTo(proxy).
+										invokeWithArguments(args);
+
+								return (result);
+							}
+							else
+							{
+								return Storage.getFilePath(settings);
+							}
+						}
+
 						if(method.getName().equals("save"))
 						{
-							final Path filePath = Storage.getFilePath(settings);
+							final Path filePath = ((Settings) proxy).getPath();
 							final String json = GSON.toJson(data);
 							Files.write(filePath, json.getBytes());
 							return null;
 						}
 						else if(method.getName().equals("load"))
 						{
-							final Path filePath = Storage.getFilePath(settings);
+							final Path filePath = ((Settings) proxy).getPath();
 							if(Files.exists(filePath))
 							{
 								Type type = new TypeToken<HashMap<String, Object>>()
