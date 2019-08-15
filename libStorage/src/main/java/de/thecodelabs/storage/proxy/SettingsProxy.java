f93a248623a41d3895c2d422d8e49a20873772c3
tobias <thinkdifferent055@gmail.com>
2019-08-15 21:10:16 +0200

    Add default value support for SettingsProxy

--- libStorage/src/main/java/de/thecodelabs/storage/proxy/SettingsProxy.java
+++ libStorage/src/main/java/de/thecodelabs/storage/proxy/SettingsProxy.java
@@ -43,7 +43,11 @@ public class SettingsProxy
 					@Override
 					public Object invoke(Object proxy, Method method, Object[] args) throws Throwable
 					{
-						if(method.getName().equals("getPath"))
+						if(method.getName().equals("toString"))
+						{
+							return "SettingsProxy$" + proxy.getClass();
+						}
+						else if(method.getName().equals("getPath"))
 						{
 							Constructor<MethodHandles.Lookup> constructor;
 							Class<?> declaringClass;
@@ -53,14 +57,12 @@ public class SettingsProxy
 							{
 								declaringClass = method.getDeclaringClass();
 								constructor = MethodHandles.Lookup.class.getDeclaredConstructor(Class.class, int.class);
-
 								constructor.setAccessible(true);
-
-								result = constructor.
-										newInstance(declaringClass, MethodHandles.Lookup.PRIVATE).
-										unreflectSpecial(method, declaringClass).
-										bindTo(proxy).
-										invokeWithArguments(args);
+								result = constructor
+										.newInstance(declaringClass, MethodHandles.Lookup.PRIVATE)
+										.unreflectSpecial(method, declaringClass)
+										.bindTo(proxy)
+										.invokeWithArguments(args);
 
 								return (result);
 							}
@@ -101,7 +103,34 @@ public class SettingsProxy
 							return null;
 						}
 
-						return data.get(method.getName());
+						Object value = data.get(method.getName());
+						if(value == null)
+						{
+							if(method.isAnnotationPresent(DefaultString.class))
+							{
+								value = method.getAnnotation(DefaultString.class).value();
+								data.put(method.getName(), value);
+							}
+
+							if(method.isAnnotationPresent(DefaultBoolean.class))
+							{
+								value = method.getAnnotation(DefaultBoolean.class).value();
+								data.put(method.getName(), value);
+							}
+
+							if(method.isAnnotationPresent(DefaultInteger.class))
+							{
+								value = method.getAnnotation(DefaultInteger.class).value();
+								data.put(method.getName(), value);
+							}
+
+							if(method.isAnnotationPresent(DefaultDouble.class))
+							{
+								value = method.getAnnotation(DefaultDouble.class).value();
+								data.put(method.getName(), value);
+							}
+						}
+						return value;
 					}
 				}
 		);
