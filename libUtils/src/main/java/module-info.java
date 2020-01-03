b6b220ba1007f7978eb624e7dc22bf5be2de7d7e
tobias <tobias@thecodedev.de>
2020-01-03 17:45:47 +0100

    Add missing exports to libUtils

--- libUtils/src/main/java/module-info.java
+++ libUtils/src/main/java/module-info.java
@@ -24,22 +24,21 @@ module de.thecodelabs.libUtils {
 	requires dom4j;
 	requires gagawa;
 
+	exports de.thecodelabs.utils.application;
+	exports de.thecodelabs.utils.application.container;
 	exports de.thecodelabs.utils.application.resources.classpath;
 	exports de.thecodelabs.utils.application.resources.remote;
 	exports de.thecodelabs.utils.application.resources.external;
-	exports de.thecodelabs.utils.application.container;
 	exports de.thecodelabs.utils.application.system;
 	exports de.thecodelabs.utils.application.update;
 
+	exports de.thecodelabs.utils.io;
+
 	exports de.thecodelabs.utils.list;
+
 	exports de.thecodelabs.utils.logger;
 
-	exports de.thecodelabs.utils.io;
 	exports de.thecodelabs.utils.threading;
-
-	exports de.thecodelabs.utils.util;
-	exports de.thecodelabs.utils.util.win;
-	exports de.thecodelabs.utils.util.zip;
 
 	exports de.thecodelabs.utils.ui;
 	exports de.thecodelabs.utils.ui.animation;
@@ -49,4 +48,10 @@ module de.thecodelabs.libUtils {
 	exports de.thecodelabs.utils.ui.scene;
 	exports de.thecodelabs.utils.ui.scene.input;
 	exports de.thecodelabs.utils.ui.size;
+
+	exports de.thecodelabs.utils.util;
+	exports de.thecodelabs.utils.util.localization;
+	exports de.thecodelabs.utils.util.localization.formatter;
+	exports de.thecodelabs.utils.util.win;
+	exports de.thecodelabs.utils.util.zip;
 }
