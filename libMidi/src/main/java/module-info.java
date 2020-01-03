035523093dce5bbb49e1f8be0bb7d8198c641a95
tobias <tobias@thecodedev.de>
2020-01-03 17:18:16 +0100

    Update libMidi to java 11

---
+++ libMidi/src/main/java/module-info.java
@@ -0,0 +1,20 @@
+module de.thecodelabs.libMidi {
+
+	requires de.thecodelabs.libUtils;
+
+	requires java.desktop;
+
+	requires gson;
+	requires coremidi4j;
+
+	exports de.thecodelabs.midi;
+	exports de.thecodelabs.midi.action;
+	exports de.thecodelabs.midi.device;
+	exports de.thecodelabs.midi.device.java;
+	exports de.thecodelabs.midi.event;
+	exports de.thecodelabs.midi.feedback;
+	exports de.thecodelabs.midi.mapping;
+	exports de.thecodelabs.midi.midi;
+	exports de.thecodelabs.midi.midi.feedback;
+	exports de.thecodelabs.midi.serialize;
+}
