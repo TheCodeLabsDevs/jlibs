561771d6b1665b364777fe3e7750fed5c89056e0
tobias <tobias@thecodedev.de>
2020-06-15 23:39:28 +0200

    Change maven url in test

--- libArtifactory/src/test/java/de/thecodelabs/artifactory/Main.java
+++ libArtifactory/src/test/java/de/thecodelabs/artifactory/Main.java
@@ -4,7 +4,7 @@ public class Main
 {
 	public static void main(String[] args)
 	{
-		Artifactory artifactory = new Artifactory("https://maven.thecodedev.de/artifactory");
+		Artifactory artifactory = new Artifactory("https://maven.thecodelabs.de/artifactory");
 		final MavenRepository repository = artifactory.getRepository("TheCodeLabs-release");
 		final Folder folder = repository.getFolder("de/thecodelabs/jlibs");
 		final File file = repository.getFile("de/thecodelabs/libUtils/3.0.0/libUtils-3.0.0.jar");
