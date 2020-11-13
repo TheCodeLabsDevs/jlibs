36f0076f640fa1ce5184c3cdcd59d4c5c5ec7fa6
tobias <thinkdifferent055@gmail.com>
2020-11-13 21:18:12 +0100

    libPlugins: Update to version 3.0.5
    - get plugin descriptor

--- libPlugins/libPlugins-core/src/main/java/de/thecodelabs/plugins/Plugin.java
+++ libPlugins/libPlugins-core/src/main/java/de/thecodelabs/plugins/Plugin.java
@@ -5,4 +5,12 @@ public interface Plugin
 	void startup(PluginDescriptor descriptor);
 
 	void shutdown();
+
+	default PluginDescriptor getPluginDescriptor()
+	{
+		final ClassLoader classLoader = getClass().getClassLoader();
+		if(classLoader instanceof PluginClassLoader)
+			return ((PluginClassLoader) classLoader).getPluginDescriptor();
+		return null;
+	}
 }
